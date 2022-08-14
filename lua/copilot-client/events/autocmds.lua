213540d6706060507ffa2810a6a9ad45a570e247
samodostal <samuel.dostal.sk@gmail.com>
2022-08-14 14:24:02 +0200

    fix: missing augroup

--- lua/copilot-client/events/autocmds.lua
+++ lua/copilot-client/events/autocmds.lua
@@ -4,7 +4,13 @@ local keymaps = require("copilot-client.events.keymaps")
 local M = {}
 
 M.create_autocmds = function(opts)
+	local augroup = vim.api.nvim_create_augroup(
+		"CopilotClient",
+		{ clear = true }
+	)
+
 	vim.api.nvim_create_autocmd({ "InsertLeave" }, {
+		group = augroup,
 		callback = function()
 			keymaps.delete_keymaps(opts)
 		end,
@@ -14,6 +20,7 @@ M.create_autocmds = function(opts)
 	vim.api.nvim_create_autocmd(
 		{ "InsertLeave", "TextChangedI", "CursorMovedI" },
 		{
+			group = augroup,
 			callback = function()
 				ui.clear_ghost_completion()
 			end,
